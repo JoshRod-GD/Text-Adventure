16eea4af5e02220870fed48d4bafb253b8ef01ae
JoshRod-GD <80925863+JoshRod-GD@users.noreply.github.com>
2021-11-09 22:30:50 -0800

    Finished project just need to fix css a bit

--- js/site.js
+++ js/site.js
@@ -28,8 +28,8 @@ storymode =  {
     horse: {
         name: "Preparing the horse",
         description: "As you fix your saddle onto your horse, you hear a shout in the distance, 'Master! Wait Master!'\
-                      A young, adolescent boy  around the age of 15 runs up to you, and falls at your feet.  Master please! Take me with you! I can help I have been\
-                      training with my sword as you taught me. I will not  get in the way this time, I swear it on the great crystal",
+                      A young, adolescent boy  around the age of 15 runs up to you, and falls at your feet.  'Master please! Take me with you! I can help I have been\
+                      training with my sword as you taught me. I will not  get in the way this time, I swear it on the great crystal'",
 
         exitKeys: ['boy', 'challenge'],
         exitTexts: ["Allow The boy to accompany you",
@@ -50,7 +50,7 @@ storymode =  {
           name: "Challenge the boy",
           description: "As it turns out, the young squire was not exaggerating his newfound abilities. His shortsword sliced your ribcage as you moved\
             	to block the opposite side. You felt the warm blood drench your clothes as the boy fell to your side. He was shouting at you but his words became\
-            	unintelligible as consciousness began to slip away from you. Who will save the kingdom now",
+            	unintelligible as consciousness began to slip away from you. Who will save the kingdom now?",
 
           exitKeys: ['start'],
           exitTexts: ["The Kingdom needs you Adventurer, Try again?"]
@@ -63,9 +63,42 @@ storymode =  {
 
       exitKeys: ['callout', 'flee'],
       exitTexts: ["Call out to the witch", "Flee with your squire"]
-
     },
 
+    callout: {
+      name: "Calling the Witch",
+      description: "'You there!', you call out to the witch. Could the shining gem be the King's crystal? \
+      The witch hisses and responds, 'How dare you speak to me you lowly human!' You draw your sword and charge toward the witch.\
+       The hilt of your sword is warm and with a quick glance you notice a blue glow emanating from the entire sword.\
+        This must be why you were the chosen one! Surely this power was meant to defeat this evil witch and retrieve the King's crystal. \
+        The witch starts to dissipate into a shadow once more, but not before your sword makes contact with her throat, dealing a fatal blow.\
+         You snatch the crystal from her hands and ride back to where your squire is cowering by a tree.\
+       'Come boy, we must return the King's crystal at once. The fate of the kingdom depends on it!'",
+
+
+      exitKeys: ['home'],
+      exitTexts: ["Return to the Kingdom with the Crystal"]
+    },
+
+    flee: {
+      name: "Run for your life!",
+      description: "'I see the champion I was warned of is nothing but a coward!', the witch screamed out.\
+       You run as fast as your feet will carry you, but a root catches your foot. As you fall you catch a glimpse of your squire \
+       running in the distance before the shadows engulf you completely and your last breath escapes you...",
+
+      exitKeys: ['start'],
+      exitTexts: ["The Kingdom needs you Adventurer, Try again?"]
+     },
+
+    home: {
+      name: "Home Sweet Home!",
+      description: "As you return home with the crystal  the king and his small milita of guards are at the gate as they were informed of your arival, you hand the crystal back to the king \
+                    and  in the throne room he makes the grand announcement that you are being given the title of grand knight meanwhile the squire  being a 15 year old boy  is being given\
+                    riches for him and his family as well as his own title as knight in training where he will continue to serve under you as an apprentice.",
+
+      exitKeys: ['start'],
+      exitTexts: ["The end"]
+     },
 
 
     // Forest path
@@ -83,7 +116,7 @@ storymode =  {
       name: "Straight Path",
       description: " You decide to follow the path that leads straight ahead, traveling halfway through the path you hear chains begin to rattle and within the darkness you see a wraith\
                 begin to float towards you, you attempt to run past it  and almost reaching the edge of the darkness you feel your body being dragged back  into the forest  and the wraith grabs hold of you begining to\
-                drain your life out from your body and as your life begins to fade the last thing you see is your weapons hitting the ground and nothing but utter darkness",
+                drain your life out from your body and as your life begins to fade the last thing you see is your weapons hitting the ground and nothing but utter darkness.",
 
       exitKeys: ['start'],
       exitTexts: ["The Kingdom needs you Adventurer, Try again?"]
@@ -92,19 +125,19 @@ storymode =  {
     path2: {
       name: "Curved Path",
       description: " In the end you decide to follow the Curved path and it leads you to  the other side of the forest where behind you life is filling the forest while in front of you\
-                  there is dead trees as if they have been dead for years as well as there being a strong mist  in the surrounding area. from the distance you see a hideout wher there was one single gaurd protracting a strage door\
-                   you approach the gaurd whom you realize is not friendly  once you battle the gaurd your sword slicking his head clean off his torso reveals a portal behind the closed door you have a strong feeling that the crystal\
-                   is behind that portal but you can also see inside there are  many gaurds behind it along with a dark knight at the end of the large room  what is the best plan? you tell yourself...",
+                  there is dead trees as if they have been dead for years as well as there being a strong mist  in the surrounding area. from the distance you see a hideout wher there was one single guard protecting a strange door\
+                   you approach the guard whom you realize is not friendly  once you battle the guard your sword slicing his head clean off his torso reveals a portal behind the closed door, you have a strong feeling that the crystal\
+                   is behind that portal but you can also see inside there are  many guards behind it along with a dark knight at the end of the large room  'what is the best plan?' you think to yourself...",
 
-      exitKeys: ['loud', 'quiet'],
+      exitKeys: ['loud', 'quietly'],
       exitTexts: ["Go into the portal loud", "Go into the portal quietly"]
     },
 
     loud: {
        name: "Loud and Proud",
-       description: " You decide to go in loud and take on the army of shadow gaurds  as you ready your weapons  you charge in to the portal screaming LEROY JENKINS!!!... the gaurds all laugh at you  seeing that you charge into their\
-                  home base screaming such rediculous words  two of the guards capture you and take you over to the dark knight who executes you in front of all the gaurds  and posting your head on a stake outside the portals door as an\
-                  example of what happens to those who attempt to tamper with them...You Are Dead",
+       description: " You decide to go in loud and take on the army of shadows  as you ready your weapons  you charge in to the portal screaming LEROY JENKINS!!!... the guards all laugh at you  seeing that you charge into their\
+                  home base screaming such ridiculous words  two of the guards capture you and take you over to the dark knight who executes you in front of all the guards  and posting your head on a stake outside the portal door as an\
+                  example of what happens to those who attempt to tamper with them...You Are Dead.",
 
        exitKeys: ['start'],
        exitTexts: ["The Kingdom needs you Adventurer, Try again?"]
@@ -112,9 +145,9 @@ storymode =  {
 
     quietly: {
        name: "Quiet as a mouse",
-       description: " You decide to enter in quietly and  swifty as a mouse, As you enter the portal  you decide to take out each shadow gaurd one by one as they become descrated by other events happening in the room\
-                      once all gaurds are delt with  you realized that to take out the dark knight you must use a differnt weapon from that of your own, you see the shadow guards had special magic infused swords and axes\
-                    you battle the dark knight with one of these weapons and eventually managed to land a deadly blow onto his chest the dark knight falls to the ground allowing you to collect the crystal...after a long hard journey\
+       description: " You decide to enter in quietly and  swifty as a mouse, As you enter the portal  you decide to take out each shadow guard one by one as they are distcrated by other events happening in the room\
+                      once all guards are delt with  you realized that to take out the dark knight you must use a different weapon from that of your own, you see the shadow guards had special magic infused swords and axes\
+                    you battle the dark knight with one of these weapons and eventually managed to land a deadly blow onto his chest, the dark knight falls to the ground allowing you to collect the crystal,after a long hard journey\
                     you decide to steal the dark knights horse and ride back into the kingdom.",
 
        exitKeys: ['ending'],
@@ -125,7 +158,7 @@ storymode =  {
           name: "Retun of a hero!",
           description: " As you ride back into the kingdom enterence the people of it begin to cheer in joy that their beloved crystal is back to its rightful place, once you reach the castle gate  the king is there in the\
                       courtyard, As you hand the crystal back to the king  he is extreamly greatful for your service to the kingdom and for it offers to you a  request to begin a adventurers guild funded by the king for many more\
-                      as yourself could come togehter and  seek out greater adventurs  the king also mentioned that he would like you to lead this guild into greatness.",
+                      as yourself could come together and  seek out greater adventures,the king also mentioned that he would like you to lead this guild into greatness.",
 
          exitKeys: ['start'],
          exitTexts: ["The end"]
@@ -138,7 +171,7 @@ storymode =  {
     different: {
           name: "Taking a different route",
           description: " You decide that going the direct route towards castle territory would be just what the enemy is expecting, you know of a secret shortcut through the forest along the stream that takes you to the castle faster.\
-                      While walking through the shortcut, you are ambushed by two dark sorcerers, materialized from shadows, who know powerful magic. They cast a spell towards you, how do you act",
+                      While walking through the shortcut, you are ambushed by two dark sorcerers, materialized from shadows, who know powerful magic. They cast a spell towards you, how will you act?",
 
          exitKeys: ['block', 'dodge'],
          exitTexts: ["Block the magic spell","Dodge the magic spell"]
@@ -146,7 +179,7 @@ storymode =  {
 
     block: {
             name: "Block the spell",
-            description: "You raise your shield and attempt to block the magic spell. It’s no good, the spell goes right through your shield, engulfing you in darkness and destroying you. ",
+            description: "You raise your shield and attempt to block the magic spell. It is no good, the spell goes right through your shield, engulfing you in darkness and destroying you. ",
 
             exitKeys: ['start'],
             exitTexts: ["The Kingdom needs you Adventurer, Try again?"]
@@ -155,7 +188,7 @@ storymode =  {
     dodge: {
           name: "Dodging the spell",
           description: "Using quick footing, you dodge the magic spell cast upon you. You manage to roll far enough away from the sorcerers that you are able to escape. The sorcerer's give chase, and end up chasing you all the\
-                      way to their lair, where the crystal is! How stupid of them, you thought! ",
+                      way to their lair, where the crystal is! 'How stupid of them' you thought! ",
 
           exitKeys: ['run', 'wayout'],
           exitTexts: ["Take the crystal and run!!", "Think of a way out"]
@@ -172,9 +205,9 @@ storymode =  {
     wayout: {
          name: "Thinking of a way out",
          description: "You wait until the sorcerer's attack, being alert of your surroundings. You suddenly hear something rumbling above,\
-                    it was a trap! You quickly grab the crystal and hold it, standing under the falling boulders. The sorcerer’s lunge at you trying to get the crystal back before\
-                    it is destroyed along with you. At the last second, you jump out of the way, leaving the sorcerer’s to be crushed...\
-                    The crystal, grasped in your hand, is safe, for now. Let’s head to the castle and tell the king the good news.",
+                    it was a trap! You quickly grab the crystal and hold it, standing under the falling boulders. The sorcerer's lunge at you trying to get the crystal back before\
+                    it is destroyed along with you. At the last second, you jump out of the way, leaving the sorcerer's to be crushed...\
+                    The crystal, grasped in your hand, is safe, for now. Let's head to the castle and tell the king the good news.",
 
          exitKeys: ['ending3'],
          exitTexts: ["Return to the kingdom"]
@@ -183,7 +216,7 @@ storymode =  {
     ending3: {
         name: "Welcome Back!",
         description: " As you return the crystal to the king everyone including the townsfolk are cheering with happiness  that night after returning the crystal back into its rightful hands the\
-                    king himself decided that they would have a feast as a celebration  honoring you  and the return of the crystal",
+                    king himself decided that they would have a feast as a celebration  honoring you  and the return of the crystal.",
 
         exitKeys: ['start'],
         exitTexts: ["The end"]
@@ -200,7 +233,7 @@ storymode =  {
   }
 
   function clearDisplayArea() {
-       document.getElementById(outputHTML).innerHTML = "";
+       $(outputHTML).empty();
     }
   //displays current path on screen
   function displayCurrentPath(currentPath) {
